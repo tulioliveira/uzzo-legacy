b22b0e55c8f2f7727327a17b7615428523702721
Túlio Oliveira <tulio.ao@gmail.com>
2020-05-30 13:03:15 -0300

    FIX: update body scrollbar and repo configs

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ThemeProvider from 'src/components/ThemeProvider'
+import View from 'src/components/View'
 import 'src/index.css'
 
 const Layout = ({ children }) => {
   return <ThemeProvider>
-    {children}
+    <View maxWidth="1400px" margin="0px auto">
+      {children}
+    </View>
   </ThemeProvider>
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Button from 'src/components/Button'
 import Layout from 'src/Layout'
 import Text from 'src/components/Text'
 import View from 'src/components/View'
-import Input from 'src/components/Input'
 import Tab from 'src/components/Tab'
 import LandingPage from 'static/images/landing.svg'
 import { Link } from 'gatsby'
@@ -20,14 +19,14 @@ export default function Home() {
         <Tab to="/" label="Contato" />
       </View>
     </View>
-    <View flexDirection={['column', 'row', 'row', 'row']} alignItems="center" justifyContent="space-between" px={6} py={6}>
+    <View flexDirection={['column', 'row', 'row', 'row']} alignItems="center" justifyContent="space-between" px={4} py={6}>
       <View flex={1} alignItems={['center', 'flex-start']} maxWidth="800px">
         <Text textAlign={['center', 'left']} type="h1" mb={1}>Uzzo Marcenaria</Text>
         <Text textAlign={['center', 'left']} my={2}>Acrílico, cristal, couro, metal, porcelana. Desde 1973 fornecendo sofisticação, luxo e modernidade em projetos comerciais e residenciais.</Text>
         <Button type="primary">Faça seu Projeto</Button>
       </View>
       <View flex={1} alignItems="center" justifyContent="center" my={[6, 0]}>
-        <img src={LandingPage} width="90%" />
+        <img src={LandingPage} width="95%" alt="Splash Art" />
       </View>
     </View>
   </Layout>
